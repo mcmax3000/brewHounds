39718c48f283773e40956cbc04875f42d63d87c7
mcmax3000 <djmpiggott@sympatico.ca>
2015-07-06 17:56:18 -0400

    Add to Cart Functionality Added

--- BrewHounds/Controllers/ShoppingCartsController.cs
+++ BrewHounds/Controllers/ShoppingCartsController.cs
@@ -47,7 +47,6 @@ namespace BrewHounds.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,ItemID,Quantity")] ShoppingCart shoppingCart)
         {
             if (ModelState.IsValid)
